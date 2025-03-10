8d2ab72c7754e972d18816b1a962a27626ce06e6
BRANDSTC0704 <christian.brandstaetter@fcc-group.at>
2025-03-10 17:18:56 +0100

    minor updates - mainly admin site

--- spreadsheet_app/data_entry/admin.py
+++ spreadsheet_app/data_entry/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Employee, WorkCategory, WorkHours, Count, Protocol
+
+
+class EmployeeAdmin(admin.ModelAdmin):
+  list_display = ("created_at", "first_name", "surname", "work_start","work_end", "break_time", "absence")
 
 # Register your models here.
+# admin.site.register(Employee)
+admin.site.register(WorkCategory)
+admin.site.register(WorkHours)
+admin.site.register(Count)
+admin.site.register(Protocol)
+
+admin.site.register(Employee, EmployeeAdmin)
--- spreadsheet_app/data_entry/models.py
+++ spreadsheet_app/data_entry/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils.timezone import now
-from datetime import timedelta
+from datetime import timedelta, datetime
+
 
 class Employee(models.Model):
     first_name = models.CharField(max_length=100)
@@ -9,18 +10,28 @@ class Employee(models.Model):
     work_end = models.TimeField()
     break_time = models.FloatField(help_text="Break time in hours (e.g., 0.5 for 30 min)", default=0.0)
     absence = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True, editable=False, null=False, blank=False)
 
     def __str__(self):
-        return f"{self.first_name} {self.surname}"
+        return f"{self.created_at:%d.%m.%Y %H:%M:%S} {self.first_name} {self.surname}"
 
 class WorkCategory(models.Model):
     cleaning = models.FloatField(default=0.0, help_text="Hours spent on cleaning")
     maintenance = models.FloatField(default=0.0, help_text="Hours spent on maintenance/repair")
     interruption = models.FloatField(default=0.0, help_text="Hours of work interruption")
+    created_at = models.DateTimeField(auto_now_add=True, editable=False, null=False, blank=False)
+
+    def __str__(self):
+        return f"{self.created_at:%d.%m.%Y %H:%M:%S} {self.cleaning} {self.maintenance} {self.interruption}"
+
 
 class WorkHours(models.Model):
     start_time = models.TimeField(default=now)
     end_time = models.TimeField(default=now)
+    created_at = models.DateTimeField(auto_now_add=True, editable=False, null=False, blank=False)
+
+    def __str__(self):
+        return f"{self.created_at:%d.%m.%Y %H:%M:%S} {self.start_time} {self.end_time}"
 
     @property
     def difference(self):
@@ -44,9 +55,10 @@ class Count(models.Model):
     
     category = models.CharField(max_length=50, choices=CATEGORY_CHOICES, null=True)
     count = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True, editable=False, null=False, blank=False)
 
     def __str__(self):
-        return f"{self.category}: {self.count}"
+        return f"{self.created_at:%d.%m.%Y %H:%M:%S} {self.category}: {self.count}"
 
 class Protocol(models.Model):
     protocollist = models.TextField()
